85feaf2282cdec589f09139c2106b3623731d904
seokkuuu <seokkub@gmail.com>
2023-08-20 20:44:46 -0400

    Migrated to express 4.18.2 and updated old commonjs code to esm

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-const express = require('express')
-const path = require('path')
+import express from "express";
+import { fileURLToPath } from "url";
+import { join, dirname } from "path";
 
-const app = express()
+// Create an Express app
+const app = express();
+const serverPort = 5000;
 
+// Get the directory path of the current module
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// Define a route to handle incoming requests
 app.get("/:id", (req, res) => {
-    const inputs = JSON.parse(req.params.id)
-    let newInputs = []
+  // Parse the input data from the request parameter
+  const inputs = req.params.id;
+  console.log("Input Data:", inputs);
 
-    for (let i = 0; i < inputs.length; i++) {
-        newInputs.push(inputs[i])
-    }
-    console.log(newInputs)
-    res.sendFile(path.join(__dirname, "index.html"))
-})
+  // Send the "index.html" file as the response
+  res.sendFile(join(__dirname, "index.html"));
+});
 
-app.listen(5000, () => console.log("Server running on http://localhost:5000"))
+// Start the server and listen on the specified port
+app.listen(serverPort, () => {
+  console.log(`Server running on http://localhost:${serverPort}`);
+  console.log("WARNING: This server collects and logs data from forms. Use with caution.");
+  console.log("DISCLAIMER: This code is for educational purposes only. The author is not responsible for misuse.");
+});
